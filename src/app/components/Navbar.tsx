b635e475edd314bd0f700fc652cb6952d795e2d4
Albert Deng <adeng21@gmail.com>
2024-03-05 15:58:22 -0800

    update name

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 const NavBar = () => {
   return (
     <nav className="sticky top-0 w-full">
-      <div className="flex justify-between px-4 md:px-20 md:py-0">
+      <div className="flex px-4 md:px-20 md:py-0 items-center">
         <Image src="/logo.png" alt="logo" width={100} height={100} />
-        <span className="font-bold text-lg">FSBO</span>
+        <span className="font-bold text-lg italics">ListSolo</span>
       </div>
     </nav>
   );
